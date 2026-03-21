64f0d74a3dbc2341c2e3295993d1c155dce718af
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2026-03-22 01:18:08 +0530

    implement Product entity✅

---
+++ athukorala-backend/inventory-system/src/main/java/com/athukorala/inventory_system/entity/Product.java
@@ -0,0 +1,20 @@
+package com.athukorala.inventory_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private Double price;
+    private Integer stockQuantity;
+    private String description;
+    private String imageUrl;
+}
